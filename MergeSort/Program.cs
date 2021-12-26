2b34fc7135dbfe59e7e9ab01aa3d3eb1c4f96c84
yilmazelif97 <elifyilmaz587@gmail.com>
2021-12-26 15:52:09 +0300

    Add project files.

---
+++ MergeSort/Program.cs
@@ -0,0 +1,22 @@
+﻿
+
+
+[16,21,11,8,12,22] 
+
+// merge sort mantığı şöyle: dizi daha küçük parçalara ayrılır, birleştirme işlemi yaparken sıralama yapılır
+
+      16,21,11           8,12,22  dizi daha küçük parçalara ayrılır
+
+    16,21   11         8,12     22
+
+    16  21   11        8  12   22  birleştirme yapıılırken tek tek dizi elemanları içerisinde kıyaslama yapılır zaten en küçük yapıda olduğundan 2 li kıyaslama olur
+
+    16,21   11            8,12   22
+
+     11,16,21               8,12,22
+
+             8,11,12,16,21,22   --> merge sort uygulanmış hali
+
+     Big O notation --> O(n)
+
+             n elemanlı dizide ki n tane eleman kıyaslanır, n eleman üzerinden işlem yapıldığından O(n) olur
